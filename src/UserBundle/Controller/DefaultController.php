10a45d8e6c26123e70f6923548b56aa20bde995d
Julien Brandin <julien.brandin@supinternet.fr>
2017-09-14 11:25:50 +0200

    add profil

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -4,6 +4,12 @@ namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use UserBundle\Entity\Profil;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
@@ -15,14 +21,11 @@ class DefaultController extends Controller
         return $this->render('UserBundle:Default:index.html.twig');
     }
 
-
-
     /**
      * @Route("/profil")
      */
-    public function profilAction()
+    public function profilAction(Request $request)
     {
-
 
         $user = $this->getUser();
 
@@ -32,11 +35,47 @@ class DefaultController extends Controller
 
         $em = $this->getDoctrine()->getManager();
 
-        //$matters = $em->getRepository('UserBundle:matter')->findAll();
+        $profil = $em->getRepository('UserBundle:Profil')->findOneByUser($user);
+
+        if(!$profil)
+            $profil = new Profil();
 
 
+        $form = $this->createFormBuilder($profil)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('cellphone', TextType::class)
+            ->add('adress', TextType::class)
+            ->add('country', TextType::class)
+            ->add('gender', TextType::class)
+            ->add('city', TextType::class)
+            ->add('zip_code', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Editer le profil'))
+            ->getForm();
 
-        return $this->render('UserBundle:Default:index.html.twig');
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $profil = $form->getData();
+
+            $profil->setUser($user);
+
+            /*
+            echo "<pre>";
+            print_r($profil);
+            echo "</pre>";
+            die();
+            */
+
+            $em->persist($profil);
+            $em->flush();
+        }
+
+
+        return $this->render('UserBundle:Default:new.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
 
