3a9bf7a3f987aee241d56a1d1a923575bd1ae3fb
Julien Brandin <julien.brandin@supinternet.fr>
2017-11-22 17:39:32 +0100

    edit style of index

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -22,7 +22,7 @@ class DefaultController extends Controller
         $user = $this->getUser();
 
         if ($user == null){
-            return $this->redirect($this->generateUrl('fos_user_security_login'));
+            //return $this->redirect($this->generateUrl('fos_user_security_login'));
         }
 
         $em = $this->getDoctrine()->getManager();
