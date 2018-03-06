9ed37cf91ce93d2fe7d4d95ca55ec8f565ef2744
Julien Brandin <julien.brandin@supinternet.fr>
2018-03-06 14:34:59 +0100

    toto push

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,5 +10,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class DefaultController extends Controller
 {
 
+    /**
+     * Lists all advert entities.
+     *
+     * @Route("/somePoints", name="somePoints")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+
+        return $this->render('AppBundle::somePoints.html.twig');
+    }
+
 
 }
